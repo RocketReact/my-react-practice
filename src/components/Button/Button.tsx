73a76b9361ebf0f970a35f02775c6cfd608c2276
Oleg Shapoval <olegkazhan@gmail.com>
2025-08-09 13:51:36 +0300

    add onClick random color to background btn

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx';
 import css from './Button.module.css';
+import { BiSolidLogInCircle } from 'react-icons/bi';
+import { useState } from 'react';
 
 interface ButtonProps {
   variant?: 'primary' | 'secondary';
@@ -7,10 +9,34 @@ interface ButtonProps {
 }
 
 export default function Button({ variant, text }: ButtonProps) {
+  const [clickCount, setClickCount] = useState<number>(0);
+  const [randomNumber, setRandomNumber] = useState<string | null>(null);
+
+  function generateRandomNumber() {
+    const randomNumber = Math.floor(Math.random() * 10000);
+    setRandomNumber(`#${randomNumber.toString()}`);
+  }
+
+  function handleClickBtn(): void {
+    setClickCount(prev => prev + 1);
+    generateRandomNumber();
+  }
+
   return (
     <>
-      <button className={clsx(css.button, variant && css[variant])}>
-        {text}
+      <button
+        onClick={handleClickBtn}
+        style={{ backgroundColor: `${randomNumber}` }}
+        className={clsx(css.button, variant && css[variant])}
+      >
+        {text} {clickCount} {randomNumber}
+        <BiSolidLogInCircle
+          style={{
+            margin: '0 0 0 8px',
+            verticalAlign: 'middle',
+            fontSize: '20px',
+          }}
+        />
       </button>
     </>
   );

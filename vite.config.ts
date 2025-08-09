be49595420976deb43380db3e3bbf0a78ed54dc8
Oleg Shapoval <olegkazhan@gmail.com>
2025-08-09 15:52:51 +0300

    fix vite.config.ts base for deploy to GitHub & Versel

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-react-practice/',
+  base:
+    process.env.NODE_ENV === 'production'
+      ? process.env.VERCEL
+        ? '/'
+        : '/my-react-practice/'
+      : './',
   build: {
     sourcemap: true,
   },
